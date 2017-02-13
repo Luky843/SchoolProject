f7e0c02c343b5f5611e989d61639c2d750e159b5
Luky843 <lukas.oros@akademiasovy.sk>
2017-02-13 14:05:27 +0100

    text transform uppercase

--- js/Tablescript.js
+++ js/Tablescript.js
@@ -3,7 +3,7 @@ charset = "utf-8";
 function Create() {
     //Name Validator
     var Name = document.getElementById('Name');
-    var filter = /^[A-Z][a-zA-Z']+[ ]+[A-Z][a-zA-Z'\- ]*$/;
+    var filter = /^[A-Z][a-zA-Z']+[ ]+[A-Z|a-z][a-zA-Z'\- ]*$/;
     if (!filter.test(Name.value)) {
         document.getElementById("er1").innerHTML = "Enter Name and Surname!";
         document.getElementById("er1").style.color = "red";
@@ -28,8 +28,9 @@ function Create() {
             document.getElementById("EmErr").style.display = "none";
 
             //Age Validator
+
             var age = document.getElementById('Age').value;
-            
+
             if (!isInputDateValid(age)) {
                 document.getElementById("AgeErr").innerHTML = "Wrong Date!";
                 document.getElementById("AgeErr").style.color = "red";
@@ -58,7 +59,7 @@ function Create() {
                 var row = table.insertRow(table.rows.length);
                 var tabl = table.length;
                 var cell1 = row.insertCell(0);
-                cell1.innerHTML = document.getElementById('Name').value;
+                cell1.innerHTML = '<b><span class="bigger">' + document.getElementById('Name').value; + '</span></b>';
                 var cell2 = row.insertCell(1);
                 cell2.innerHTML = document.getElementById('Email').value;
                 var cell3 = row.insertCell(2);
@@ -73,26 +74,26 @@ function Create() {
         }
     }
 }
-    /*
-      var Name = document.getElementById('Name');
-      var filter = /^[A-Z][a-zA-Z']+[ ]+[A-Z][a-zA-Z'\- ]*$/;
-  
-      if (!filter.test(Name.value)) {
-          swal({
-              title: "Oops...",
-              text: "Enter name and surname!",
-              type: "error",
-              confirmButtonColor: "#AD1457",
-              confirmButtonText: "OK, I understand",
-          }
-  
-           );
-          Name.focus;
-          return false;
-        
-          return true;
-      }*/
-function isInputDateValid(date){
+/*
+  var Name = document.getElementById('Name');
+  var filter = /^[A-Z][a-zA-Z']+[ ]+[A-Z][a-zA-Z'\- ]*$/;
+
+  if (!filter.test(Name.value)) {
+      swal({
+          title: "Oops...",
+          text: "Enter name and surname!",
+          type: "error",
+          confirmButtonColor: "#AD1457",
+          confirmButtonText: "OK, I understand",
+      }
+
+       );
+      Name.focus;
+      return false;
+    
+      return true;
+  }*/
+function isInputDateValid(date) {
     var actual_date = new Date()
     var patern = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
     if (!patern.test(date)) {
@@ -119,7 +120,7 @@ function isInputDateValid(date){
         month = res[1];
         year = res[2];
     }
-    new_date = new Date(Number(year), Number(month)-1, Number(day));
+    new_date = new Date(Number(year), Number(month) - 1, Number(day));
     if (new_date >= actual_date) {
         return false;
     }
@@ -127,20 +128,20 @@ function isInputDateValid(date){
 }
 
 
-    function Alert(r) {
-        swal({
-            title: "Are you sure?",
-            text: "You will not be able to recover this file!",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#F44336",
-            confirmButtonText: "Yes, delete it!",
-            closeOnConfirm: false
-        },
-        function deleteRow() {
-            var i = r.parentNode.parentNode.rowIndex;
-            document.getElementById("MyTable").deleteRow(i);
-            swal("Deleted!", "Current row has been deleted.", "success");
+function Alert(r) {
+    swal({
+        title: "Are you sure?",
+        text: "You will not be able to recover this file!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#F44336",
+        confirmButtonText: "Yes, delete it!",
+        closeOnConfirm: false
+    },
+    function deleteRow() {
+        var i = r.parentNode.parentNode.rowIndex;
+        document.getElementById("MyTable").deleteRow(i);
+        swal("Deleted!", "Current row has been deleted.", "success");
 
-        });
-    }
+    });
+}
